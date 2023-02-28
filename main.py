5cfe9e14fee27428951abe8636a23d0471f67f49
Danil <Danilshkr@gmail.com>
2023-03-01 00:12:45 +0700

    table

--- main.py
+++ main.py
@@ -27,9 +27,23 @@ def distrib():
     people = ['Ридли Скотт', 'Энди Уир', 'Марк Уотни', 'Венкат Капут', 'Тедди Сандерс', 'Шон Бин']
     return render_template('places.html', people=people, title='Распределение')
 
+
 @app.route('/table/<pol>/<int:age>')
 def table(pol, age):
-    return render_template('table.html', pol=pol, age=age)
+    pict = {
+        'pink': url_for('static', filename='img/pink.png'),
+        'red': url_for('static', filename='img/red.png'),
+        'green': url_for('static', filename='img/green.png'),
+        'purple': url_for('static', filename='img/purple.png')
+    }
+    people = {
+        'boy': url_for('static', filename='img/boy.png'),
+        'man': url_for('static', filename='img/man.png'),
+        'girl': url_for('static', filename='img/girl.png'),
+        'woman': url_for('static', filename='img/woman.png')
+    }
+    return render_template('table.html', pol=pol, age=age, urls=pict, people=people,
+                           style=url_for('static', filename='css/style.css'))
 
 
 if __name__ == '__main__':
